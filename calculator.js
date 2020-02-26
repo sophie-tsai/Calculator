21ab2b9cd9ca6d14bf262581432e55ff203b21ec
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-02-26 14:05:20 -0800

    Added eventListeners & Checked if Display is 0

--- calculator.js
+++ calculator.js
@@ -1,5 +1,5 @@
 /*
-1. If a operator is click, clicking another one will replace the current one
+1. If a operator is clicked, clicking another one will replace the current one
 1a. "-" does not replace operator
 2. When AC is pressed, both formulaDisplayText and numberDisplayText are cleared or reset 
 3. "=" evaluates the formula
@@ -9,5 +9,50 @@
 5. if formula is illogical, then return NAN 
 */
 
-//first - set nums and decimal
+//initialize both displays
+const numberDisplayText = document.querySelector('#numberDisplayText');
+numberDisplayText.innerText = 0;
+const formulaDisplayText = document.querySelector('#formulaDisplayText');
+
+//et nums and decimal
+const oneButton = document.querySelector('#oneButton');
+const twoButton = document.querySelector('#twoButton');
+const threeButton = document.querySelector('#threeButton');
+const fourButton = document.querySelector('#fourButton');
+const fiveButton = document.querySelector('#fiveButton');
+const sixButton = document.querySelector('#sixButton');
+const sevenButton = document.querySelector('#sevenButton');
+const eightButton = document.querySelector('#eightButton');
+const nineButton = document.querySelector('#nineButton');
+const zeroButton = document.querySelector('#zeroButton');
+const decimalButton = document.querySelector('#decimalButton');
+/*
+const numbersBank = {
+    zeroButton: 0,
+    oneButton: 1,
+    twoButton: 2,
+    threeButton: 3,
+    fourButton: 4,
+    fiveButton: 5,
+    sixButton: 6,
+    sevenButton: 7,
+    eightButton: 8,
+    nineButton: 9
+};
+*/
+const theseAreNums = [zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton];
 //use strings to store the numbers so you can concatanate/append the numbwer
+
+//set event listener for each number & event listener function
+for(let i=0; i<theseAreNums.length; i++){
+    theseAreNums[i].addEventListener("click", function () {
+        var numberClicked = theseAreNums[i].innerText;
+        console.log(numberClicked);
+        
+        //check if 0
+        if(numberDisplayText.innerText == 0){
+            numberDisplayText.innerText = numberClicked;
+            return;
+        }
+    });
+}
