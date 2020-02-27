ebb0cc8e24a298e22cc80d9b355dc120d531d528
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-02-26 21:42:29 -0800

    Cleaned Up Code and Added 'Digit Limit Met' Feature

--- calculator.js
+++ calculator.js
@@ -8,51 +8,73 @@
     use boolean to check if in "decimalmode"
 5. if formula is illogical, then return NAN 
 */
+//use strings to store the numbers so you can concatanate/append the number
+"use strict"
 
-//initialize both displays
-const numberDisplayText = document.querySelector('#numberDisplayText');
-numberDisplayText.innerText = 0;
-const formulaDisplayText = document.querySelector('#formulaDisplayText');
+//(function iife(){
 
-//et nums and decimal
-const oneButton = document.querySelector('#oneButton');
-const twoButton = document.querySelector('#twoButton');
-const threeButton = document.querySelector('#threeButton');
-const fourButton = document.querySelector('#fourButton');
-const fiveButton = document.querySelector('#fiveButton');
-const sixButton = document.querySelector('#sixButton');
-const sevenButton = document.querySelector('#sevenButton');
-const eightButton = document.querySelector('#eightButton');
-const nineButton = document.querySelector('#nineButton');
-const zeroButton = document.querySelector('#zeroButton');
-const decimalButton = document.querySelector('#decimalButton');
-/*
-const numbersBank = {
-    zeroButton: 0,
-    oneButton: 1,
-    twoButton: 2,
-    threeButton: 3,
-    fourButton: 4,
-    fiveButton: 5,
-    sixButton: 6,
-    sevenButton: 7,
-    eightButton: 8,
-    nineButton: 9
-};
-*/
-const theseAreNums = [zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton];
-//use strings to store the numbers so you can concatanate/append the numbwer
+function setup() {
+    const numberDisplayText = document.querySelector('#numberDisplayText');
+    numberDisplayText.innerText = 0;
+    const formulaDisplayText = document.querySelector('#formulaDisplayText');
 
-//set event listener for each number & event listener function
-for(let i=0; i<theseAreNums.length; i++){
-    theseAreNums[i].addEventListener("click", function () {
-        var numberClicked = theseAreNums[i].innerText;
-        console.log(numberClicked);
-        
-        //check if 0
-        if(numberDisplayText.innerText == 0){
-            numberDisplayText.innerText = numberClicked;
-            return;
-        }
-    });
+    const calcButtonContainers = document.querySelectorAll(".calcButtonContainer");
+    for (let container of calcButtonContainers) {
+        container.addEventListener('click', onButtonClick)
+    }
+
 }
+
+setup();
+
+function maxDigits() {
+    holdValue = numberDisplayText.innerText;
+    numberDisplayText.innerText = "DIGIT LIMIT MET";
+    setTimeout(function () {
+        numberDisplayText.innerText = holdValue;
+    }, 2500
+    );
+}
+
+let holdValue;
+function handleNumber(num) {
+    //check if num is 0
+    if (numberDisplayText.innerText == 0) {
+        numberDisplayText.innerText = num;
+    } else {
+        if (numberDisplayText.innerText.length > 21) {
+            maxDigits();
+            return;
+        } else if(numberDisplayText.innerText === "DIGIT LIMIT MET") {
+            return;
+        }  else {
+            numberDisplayText.innerText += num;
+        }
+    }
+    //else, append digit
+}
+
+function handleDot() {
+
+}
+
+function handleOperator(operator) {
+
+}
+
+function onButtonClick(event) {
+    const value = event.srcElement.innerText;
+
+    if (Number.isInteger(parseInt(value))) {
+        handleNumber(value);
+    } else if (value === '.') {
+        handleDot();
+    } else {
+        handleOperator(value);
+    }
+}
+
+//})();
+
+
+
