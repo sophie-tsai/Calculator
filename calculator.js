3d08820a1f830fea12b15960fbc645b8f223a39b
sophie-tsai <55775756+sophie-tsai@users.noreply.github.com>
2020-03-05 21:26:33 -0800

    Cleaned Up Code

--- calculator.js
+++ calculator.js
@@ -1,80 +1,102 @@
-/*
-1. If a operator is clicked, clicking another one will replace the current one
-1a. "-" does not replace operator
-2. When AC is pressed, both formulaDisplayText and numberDisplayText are cleared or reset 
-3. "=" evaluates the formula
-3a. "=" erases last operator, if any
-4. Only one (1) "." at a time
-    use boolean to check if in "decimalmode"
-5. if formula is illogical, then return NAN 
-*/
-//use strings to store the numbers so you can concatanate/append the number
-
-
-// Bugs
-
-
-//when pressing plus, it double appends the number?
-
-
 "use strict"
 
 //(function iife(){
 let hasDot;
 let isNegative;
+const DIGIT_LIMIT_MET_MSG = "DIGIT LIMIT MET";
+setup();
+
 function setup() {
+    for (let container of calcButtonContainers) {
+        container.addEventListener('click', onButtonClick);
+    }
+    reset();
+}
+
+function reset() {
     const numberDisplayText = document.querySelector('#numberDisplayText');
     numberDisplayText.innerText = "0";
     const formulaDisplayText = document.querySelector('#formulaDisplayText');
     formulaDisplayText.innerText = "";
     const calcButtonContainers = document.querySelectorAll(".calcButtonContainer");
-    for (let container of calcButtonContainers) {
-        container.addEventListener('click', onButtonClick);
-    }
     hasDot = false;
     resetToPositive();
 }
 
-setup();
-
-function resetToPositive() {
-    isNegative = false;
+function onButtonClick(event) {
+    const value = event.srcElement.innerText;
+    if (Number.isInteger(parseInt(value))) {
+        handleNumber(value);
+        return;
+    }
+    if (value === '.') {
+        handleDot();
+        return;
+    }
+    handleOperator(value);
 }
 
-function maxDigits(display) {
-    if (display === "number") {
-        holdValue = numberDisplayText.innerText;
-        function clearDigitLimitMet() {
-            numberDisplayText.innerText = holdValue;
-        }
-
-        numberDisplayText.innerText = "DIGIT LIMIT MET";
-        setTimeout(clearDigitLimitMet, 2000);
-
+function handleDot() {
+    if (hasDot) {
+        return;
     }
-    else {
-        holdValue = formulaDisplayText.innerText;
-        function clearDigitLimitMet() {
-            formulaDisplayText.innerText = holdValue;
-        }
-
-        formulaDisplayText.innerText = "DIGIT LIMIT MET";
-        setTimeout(clearDigitLimitMet, 2000);
+    if (operators.has(numberDisplayText.innerText)) {
+        return;
+        // TODO fix bug
     }
+    if (formulaDisplayText.innerText.includes("=")) {
+        return;
+        // TODO fix bug
+    }
+    numberDisplayText.innerText += ".";
+    hasDot = true;
 }
 
-let holdValue;
+const operators = new Set(["+", "-", "/", "*"]);
+
+function handleOperator(operator) {
+    if (numberDisplayText.innerText === DIGIT_LIMIT_MET_MSG) {
+        return;
+    }
+    if (operator === "AC") {
+        reset();
+        return;
+    }
+    if (operator === "±") {
+        toggleNegative();
+        return;
+    }
+
+    if (operator === "=") {
+        evaluate();
+        return;
+    }
+
+    const previouslyEvaluated = formulaDisplayText.innerText.includes("=");
+    if (previouslyEvaluated) {
+        formulaDisplayText.innerText = numberDisplayText.innerText + operator;
+        numberDisplayText.innerText = operator;
+        return;
+    }
+    hasDot = false;
+    clearAndReplace(operator);
+}
 
 function handleNumber(num) {
-    //check if num is 0
+
+    //if an operator has been pressed then replace the operator
+    if (operators.has(numberDisplayText.innerText)) {
+        numberDisplayText.innerText = "";
+    }
+
+    //comment?
     if (formulaDisplayText.innerText.includes("=")) {
         formulaDisplayText.innerText = "";
         numberDisplayText.innerText = num;
     } else if (numberDisplayText.innerText === "0") {
         numberDisplayText.innerText = num;
     } else {
-        if (numberDisplayText.innerText === "DIGIT LIMIT MET") {
-
+        if (numberDisplayText.innerText === DIGIT_LIMIT_MET_MSG) {
             return;
         } else if (numberDisplayText.innerText.length >= 10) {
             maxDigits("number");
@@ -83,80 +105,67 @@ function handleNumber(num) {
             numberDisplayText.innerText += num;
         }
     }
-    //else, append digit
 }
 
-function handleDot() {
-    if (hasDot) {
-        return;
-    } else if (operators.includes(numberDisplayText.innerText)) {
-        return;
-    }
-    else {
-        numberDisplayText.innerText += ".";
-        hasDot = true;
+function resetToPositive() {
+    isNegative = false;
+}
 
+function maxDigits(display) {
+    const CLEAR_TIME = 2000;
+    if (display === "number") {
+        const holdValue = numberDisplayText.innerText;
+        numberDisplayText.innerText = DIGIT_LIMIT_MET_MSG;
+        setTimeout(() => numberDisplayText.innerText = holdValue, CLEAR_TIME);
+    } else {
+        const holdValue = formulaDisplayText.innerText;
+        formulaDisplayText.innerText = DIGIT_LIMIT_MET_MSG;
+        setTimeout(() => formulaDisplayText.innerText = holdValue, CLEAR_TIME);
     }
 }
 
-const operators = ["+", "-", "/", "*"];
-//max length for formulaDisplayText.innerText should be 30
-
-
-
-function handleOperator(operator) {
-    if (numberDisplayText.innerText === "DIGIT LIMIT MET") {
+function toggleNegative() {
+    if (operators.has(numberDisplayText.innerText)) {
         return;
     }
-    if (operator === "±") {
-        if (operators.includes(numberDisplayText.innerText)) {
-            return;
-        }
-        if (!isNegative) {
-            let numberDisplayArray = numberDisplayText.innerText.split("");
-            numberDisplayArray.unshift("-");
-            numberDisplayText.innerText = numberDisplayArray.join("");
-            isNegative = true;
-
-        } else {
-            let numberDisplayArray = numberDisplayText.innerText.split("");
-            numberDisplayArray.shift("-");
-            numberDisplayText.innerText = numberDisplayArray.join("");
-            isNegative = false;
-
-        }
-    } else if (operator === "AC") {
-        setup();
-    } else if (operator === "=") {
-
-        resetToPositive();
-        if (operators.includes(numberDisplayText.innerText)) {
-            return;
-        }
-        if (!formulaDisplayText.innerText.includes("=")) {
-            //if "=" hasn't already been passed then evaluate the formula
-            appendNumberDisplay();
-            const formulaAnswer = eval(formulaDisplayText.innerText);
-
-            formulaDisplayText.innerText = formulaDisplayText.innerText + "=" + formulaAnswer;
-            numberDisplayText.innerText = formulaAnswer;
-        }
-    } else if (operator) {
-        if (formulaDisplayText.innerText.includes("=")) {
-            formulaDisplayText.innerText = numberDisplayText.innerText + operator;
-            numberDisplayText.innerText = operator;
-
-        } else {
-            clearAndReplace(operator);
-        }
+    if (isNegative) {
+        // TODO make this into 1 liner .replace, then flex
+        let numberDisplayArray = numberDisplayText.innerText.split("");
+        numberDisplayArray.shift("-");
+        numberDisplayText.innerText = numberDisplayArray.join("");
+        isNegative = false;
+        return;
     }
+    // TODO see if we can make this into 1 liner
+    let numberDisplayArray = numberDisplayText.innerText.split("");
+    numberDisplayArray.unshift("-");
+    numberDisplayText.innerText = numberDisplayArray.join("");
+    isNegative = true;
+}
 
+function evaluate() {
+    resetToPositive();
+    hasDot = false;
+    if (operators.has(numberDisplayText.innerText)) {
+        return;
+    }
+    if (!formulaDisplayText.innerText.includes("=")) {
+        //if "=" hasn't already been passed then evaluate the formula
+        appendNumberDisplay();
+        const formulaAnswer = eval(formulaDisplayText.innerText);
+
+        formulaDisplayText.innerText = formulaDisplayText.innerText + "=" + formulaAnswer;
+        numberDisplayText.innerText = formulaAnswer;
+        return;
+    }
 }
 
 function clearAndReplace(operator) {
     resetToPositive();
-    if (operators.includes(numberDisplayText.innerText)) {
+    if (operators.has(numberDisplayText.innerText)) {
         //check to see if there's an operator there, if there is then replace, not append
+
+        // TODO make this into 1 liner .replace, then FLEX
         numberDisplayText.innerText = operator;
         let formulaDisplayArray = formulaDisplayText.innerText.split("");
         formulaDisplayArray.pop();
@@ -171,33 +180,15 @@ function clearAndReplace(operator) {
 }
 
 function appendNumberDisplay() {
-    //do not append if digit limit met
-    if (formulaDisplayText.innerText === "DIGIT LIMIT MET") {
+    //do not append if DIGIT_LIMIT_MET_MSG
+    if (formulaDisplayText.innerText === DIGIT_LIMIT_MET_MSG) {
         return;
-    } else if (formulaDisplayText.innerText.length >= 20) {
+    }
+    if (formulaDisplayText.innerText.length >= 20) {
         maxDigits("formula");
         return;
     }
     formulaDisplayText.innerText += numberDisplayText.innerText;
 }
-
-function onButtonClick(event) {
-    const value = event.srcElement.innerText;
-
-    if (Number.isInteger(parseInt(value))) {
-        //if an operator has been pressed then replace the operator
-        if (operators.includes(numberDisplayText.innerText)) {
-            numberDisplayText.innerText = "";
-
-        }
-        handleNumber(value);
-
-    } else if (value === '.') {
-        handleDot();
-    } else {
-        handleOperator(value);
-    }
-}
-
 
 //})();
